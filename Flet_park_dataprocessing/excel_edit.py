96cb4071988769bd55e6c7c143b1fffb4e6e278f
BarkBilbo <125621373+BarkBilbo@users.noreply.github.com>
2023-12-14 08:59:36 +0900

    Update excel_edit.py

--- Flet_park_dataprocessing/excel_edit.py
+++ Flet_park_dataprocessing/excel_edit.py
@@ -1,1 +1,11 @@
-sdf
+# pip install pandas openpyxl
+
+import pandas as pd
+
+df = pd.read_excel('excel_edit.xlsx')
+
+rows_to_delete = [0,1,2,3,4,5,6,7,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+df.drop(index=rows_to_delete, inplace = True)
+
+df.columns = df.iloc[0]
+df = df[1:]
