07e2e6466bc7e129f87e10d9cc6e1fd35dfc93d4
BarkBilbo <125621373+BarkBilbo@users.noreply.github.com>
2023-12-14 09:02:49 +0900

    Update excel_edit.py

--- Flet_park_dataprocessing/excel_edit.py
+++ Flet_park_dataprocessing/excel_edit.py
@@ -9,3 +9,30 @@ df.drop(index=rows_to_delete, inplace = True)
 
 df.columns = df.iloc[0]
 df = df[1:]
+
+
+import pandas as pd
+
+
+
+# ## df1 => 계약번호, 차명, 지점, 대리점, 주행거리, 불량내용
+# ## df2 => 생산번호, 상호, 출하일, 부위구분, Nan
+# ## df3 => 차대번호, 성명, 발생일, 불량구분, Nan
+
+
+
+df1 = df.iloc[:,:2].T.reset_index()
+df2 = df.iloc[:,3:5].T.reset_index()
+df3 = df.iloc[:,6:8].T.reset_index()
+
+
+
+df11 = pd.DataFrame(columns=df1.iloc[0, :], data=[df1.iloc[1, :].values])
+df22 = pd.DataFrame(columns=df2.iloc[0, :], data=[df2.iloc[1, :].values])
+df33 = pd.DataFrame(columns=df3.iloc[0, :], data=[df3.iloc[1, :].values])
+df33
+
+
+df_final = pd.concat([df11,df22.iloc[:,:4]], axis = 1)
+df_final = pd.concat([df_final,df33.iloc[:,:4]], axis =1)
+df_final
